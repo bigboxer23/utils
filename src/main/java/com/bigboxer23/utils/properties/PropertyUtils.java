32d6b02941c315aa5efa1cf2e3e9298d73ce4547
Matt Jones <bigboxer23@gmail.com>
2024-03-12 09:39:32 -0500

    feat: add util method for safely getting body (#76)

--- src/main/java/com/bigboxer23/utils/properties/PropertyUtils.java
+++ src/main/java/com/bigboxer23/utils/properties/PropertyUtils.java
@@ -4,18 +4,15 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
 
-/**
- *
- */
-public class PropertyUtils
-{
+/** */
+public class PropertyUtils {
 	private static Properties props;
 
 	public static String getProperty(String property) {
 		if (props == null) {
 			props = new Properties();
 			try (InputStream inputStream =
-					     PropertyUtils.class.getClassLoader().getResourceAsStream("application.properties")) {
+					PropertyUtils.class.getClassLoader().getResourceAsStream("application.properties")) {
 				props.load(inputStream);
 			} catch (IOException e) {
 				System.out.println("error " + e);
