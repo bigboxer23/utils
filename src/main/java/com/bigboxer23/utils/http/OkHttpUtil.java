e20cf34bfdbb9352b59bb1a646a8ea39303f987c
Matt Jones <bigboxer23@gmail.com>
2024-03-12 09:37:35 -0500

    feat: add util method for safely getting body

--- src/main/java/com/bigboxer23/utils/http/OkHttpUtil.java
+++ src/main/java/com/bigboxer23/utils/http/OkHttpUtil.java
@@ -37,7 +37,8 @@ public class OkHttpUtil {
 		return getSynchronous(url, builderCallback, defaultClient);
 	}
 
-	public static Response getSynchronous(String url, RequestBuilderCallback builderCallback, OkHttpClient client) throws IOException {
+	public static Response getSynchronous(String url, RequestBuilderCallback builderCallback, OkHttpClient client)
+			throws IOException {
 		return getCall(url, builderCallback, client).execute();
 	}
 
@@ -54,7 +55,8 @@ public class OkHttpUtil {
 		post(url, callback, builderCallback, defaultClient);
 	}
 
-	public static void post(String url, Callback callback, RequestBuilderCallback builderCallback, OkHttpClient client) {
+	public static void post(
+			String url, Callback callback, RequestBuilderCallback builderCallback, OkHttpClient client) {
 		postCall(url, null, builderCallback, client).enqueue(callback);
 	}
 
@@ -63,12 +65,14 @@ public class OkHttpUtil {
 		return postSynchronous(url, body, builderCallback, defaultClient);
 	}
 
-	public static Response postSynchronous(String url, RequestBody body, RequestBuilderCallback builderCallback, OkHttpClient client)
+	public static Response postSynchronous(
+			String url, RequestBody body, RequestBuilderCallback builderCallback, OkHttpClient client)
 			throws IOException {
 		return postCall(url, body, builderCallback, client).execute();
 	}
 
-	private static Call postCall(String url, RequestBody body, RequestBuilderCallback builderCallback, OkHttpClient client) {
+	private static Call postCall(
+			String url, RequestBody body, RequestBuilderCallback builderCallback, OkHttpClient client) {
 		return client.newCall(runBuilderCallback(
 						new Request.Builder().url(url).post(body == null ? RequestBody.create(new byte[0]) : body),
 						builderCallback)
@@ -80,7 +84,8 @@ public class OkHttpUtil {
 		delete(url, callback, builderCallback, defaultClient);
 	}
 
-	public static void delete(String url, Callback callback, RequestBuilderCallback builderCallback, OkHttpClient client)
+	public static void delete(
+			String url, Callback callback, RequestBuilderCallback builderCallback, OkHttpClient client)
 			throws IOException {
 		deleteCall(url, builderCallback, client).enqueue(callback);
 	}
@@ -89,7 +94,8 @@ public class OkHttpUtil {
 		return deleteSynchronous(url, builderCallback, defaultClient);
 	}
 
-	public static Response deleteSynchronous(String url, RequestBuilderCallback builderCallback, OkHttpClient client) throws IOException {
+	public static Response deleteSynchronous(String url, RequestBuilderCallback builderCallback, OkHttpClient client)
+			throws IOException {
 		return deleteCall(url, builderCallback, client).execute();
 	}
 
@@ -111,12 +117,14 @@ public class OkHttpUtil {
 		return putSynchronous(url, body, builderCallback, defaultClient);
 	}
 
-	public static Response putSynchronous(String url, RequestBody body, RequestBuilderCallback builderCallback, OkHttpClient client)
+	public static Response putSynchronous(
+			String url, RequestBody body, RequestBuilderCallback builderCallback, OkHttpClient client)
 			throws IOException {
 		return putCall(url, body, builderCallback, client).execute();
 	}
 
-	private static Call putCall(String url, RequestBody body, RequestBuilderCallback builderCallback, OkHttpClient client) {
+	private static Call putCall(
+			String url, RequestBody body, RequestBuilderCallback builderCallback, OkHttpClient client) {
 		return client.newCall(runBuilderCallback(
 						new Request.Builder().url(url).put(body == null ? RequestBody.create(new byte[0]) : body),
 						builderCallback)
@@ -125,5 +133,17 @@ public class OkHttpUtil {
 
 	private static Request.Builder runBuilderCallback(Request.Builder builder, RequestBuilderCallback builderCallback) {
 		return Optional.ofNullable(builderCallback).orElse(builder1 -> builder1).modifyBuilder(builder);
+	}
+
+	public Optional<String> getBody(Response response) {
+		ResponseBody body = response.body();
+		if (body == null) {
+			return Optional.empty();
+		}
+		try {
+			return Optional.of(body.string());
+		} catch (IOException theE) {
+			return Optional.empty();
+		}
 	}
 }
