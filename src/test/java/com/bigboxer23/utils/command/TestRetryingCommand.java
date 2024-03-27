fcaaa8ecd2933d282f87161077b528d4a7300d79
Matt Jones <bigboxer23@gmail.com>
2024-03-27 16:01:37 -0500

    test: add test code for the retry command class

---
+++ src/test/java/com/bigboxer23/utils/command/TestRetryingCommand.java
@@ -0,0 +1,84 @@
+package com.bigboxer23.utils.command;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
+
+import java.io.IOException;
+import org.junit.jupiter.api.Test;
+
+/** */
+public class TestRetryingCommand {
+	@Test
+	public void testExecute() throws IOException {
+		// Single success
+		Integer[] countHolder = {0};
+		RetryingCommand.execute(
+				() -> {
+					countHolder[0] = countHolder[0] + 1;
+					return null;
+				},
+				"test");
+		assertEquals(1, countHolder[0]);
+
+		// One failure, then success
+		countHolder[0] = 0;
+		RetryingCommand.execute(
+				() -> {
+					if (countHolder[0] == 0) {
+						countHolder[0] = countHolder[0] + 1;
+						throw new IOException("bad");
+					}
+					countHolder[0] = countHolder[0] + 1;
+					return null;
+				},
+				"test",
+				0);
+		assertEquals(2, countHolder[0]);
+
+		// All failure, then exception
+		countHolder[0] = 0;
+		try {
+			RetryingCommand.execute(
+					() -> {
+						countHolder[0] = countHolder[0] + 1;
+						throw new IOException("bad");
+					},
+					"test",
+					0);
+			fail();
+		} catch (IOException e) {
+			assertEquals(2, countHolder[0]);
+		}
+
+		// A few failures, then success
+		countHolder[0] = 0;
+		RetryingCommand.execute(
+				() -> {
+					if (countHolder[0] < 2) {
+						countHolder[0] = countHolder[0] + 1;
+						throw new IOException("bad");
+					}
+					countHolder[0] = countHolder[0] + 1;
+					return null;
+				},
+				"test",
+				0,
+				3);
+		assertEquals(3, countHolder[0]);
+
+		// All failure, then exception
+		countHolder[0] = 0;
+		try {
+			RetryingCommand.execute(
+					() -> {
+						countHolder[0] = countHolder[0] + 1;
+						throw new IOException("bad");
+					},
+					"test",
+					0, 3);
+			fail();
+		} catch (IOException e) {
+			assertEquals(4, countHolder[0]);
+		}
+	}
+}
