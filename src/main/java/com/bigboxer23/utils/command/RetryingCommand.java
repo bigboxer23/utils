ab3fb7f74bbf3dd31a6619d9f1a1112855662b7c
Matt Jones <bigboxer23@gmail.com>
2024-03-27 16:03:30 -0500

    test: add test code for the retry command class (#84)

--- src/main/java/com/bigboxer23/utils/command/RetryingCommand.java
+++ src/main/java/com/bigboxer23/utils/command/RetryingCommand.java
@@ -8,22 +8,66 @@ import org.slf4j.LoggerFactory;
 public class RetryingCommand {
 	private static final Logger logger = LoggerFactory.getLogger(RetryingCommand.class);
 
-	public static <T> T execute(Command<T> command, String identifier) throws IOException {
+	/**
+	 * Run a command and retry it after a pause. Can define pause length in seconds, and number of retry attempts
+	 *
+	 * @param command command to run
+	 * @param identifier identifier to log on failures
+	 * @param waitInSeconds number of seconds to pause
+	 * @param numberOfRetriesBeforeFailure how many times to retry
+	 * @return Command's return value
+	 * @param <T>
+	 * @throws IOException
+	 */
+	public static <T> T execute(
+			Command<T> command, String identifier, long waitInSeconds, int numberOfRetriesBeforeFailure)
+			throws IOException {
 		try {
 			logger.info("Starting command " + identifier);
 			return command.execute();
 		} catch (IOException e) {
-			logger.error("error running command, attempting to retry " + identifier, e);
-			try {
-				Thread.sleep(5 * 1000); // 5 sec
-				return command.execute();
-			} catch (IOException e2) {
-				logger.error("error retrying command " + identifier, e2);
-				throw e2;
-			} catch (InterruptedException e2) {
-				logger.error("error retrying command " + identifier, e2);
+			for (int ai = 0; ai < numberOfRetriesBeforeFailure; ai++) {
+				logger.error("error running command, attempting to retry " + ai + " " + identifier, e);
+				try {
+					Thread.sleep(waitInSeconds * 1000); // 5 sec
+					return command.execute();
+				} catch (IOException e2) {
+					logger.error("error retrying command " + ai + " " + identifier, e2);
+					if (ai + 1 == numberOfRetriesBeforeFailure) {
+						throw e2;
+					}
+				} catch (InterruptedException e2) {
+					logger.error("error retrying command " + identifier, e2);
+				}
 			}
 		}
 		return null;
+	}
+
+	/**
+	 * Run a command and retry it after a pause. Can define pause length in seconds. Will retry once
+	 *
+	 * @param command command to run
+	 * @param identifier identifier to log on failures
+	 * @param waitInSeconds number of seconds to pause
+	 * @return Command's return value
+	 * @param <T>
+	 * @throws IOException
+	 */
+	public static <T> T execute(Command<T> command, String identifier, long waitInSeconds) throws IOException {
+		return execute(command, identifier, waitInSeconds, 1);
+	}
+
+	/**
+	 * Run a command and retry it after a 5-second pause. Will retry once
+	 *
+	 * @param command command to run
+	 * @param identifier identifier to log on failures
+	 * @return Command's return value
+	 * @param <T>
+	 * @throws IOException
+	 */
+	public static <T> T execute(Command<T> command, String identifier) throws IOException {
+		return execute(command, identifier, 5);
 	}
 }
