4ec59c0785d73445a18eadeae27a655498c99997
Matt Jones <bigboxer23@gmail.com>
2024-09-15 21:27:20 -0500

    feat: add interface with some common time constants (#122)

---
+++ src/main/java/com/bigboxer23/utils/time/ITimeConstants.java
@@ -0,0 +1,15 @@
+package com.bigboxer23.utils.time;
+
+/**
+ *
+ */
+public interface ITimeConstants
+{
+	public static final long MINUTE = 1000 * 60;
+
+	public static final long FIFTEEN_MINUTES = MINUTE * 15;
+
+	public static final long HOUR = MINUTE * 60;
+
+	public static final long DAY = HOUR * 24;
+}
