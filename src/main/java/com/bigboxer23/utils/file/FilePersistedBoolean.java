51b2694f9354d54ae0373459454ff43d63981dc7
Matt Jones <bigboxer23@gmail.com>
2024-09-17 21:04:00 -0500

    feat: add file persisted boolean class (#124)

---
+++ src/main/java/com/bigboxer23/utils/file/FilePersistedBoolean.java
@@ -0,0 +1,32 @@
+package com.bigboxer23.utils.file;
+
+/** */
+public class FilePersistedBoolean extends AbstractFilePersisted {
+	private boolean persisted = false;
+
+	protected FilePersistedBoolean(String fileName) {
+		super(fileName);
+		persisted = getBooleanFromFile();
+		logger.info(fileName + ": initialized with value " + persisted);
+	}
+
+	private boolean getBooleanFromFile() {
+		try {
+			String fileContent = getStringFromFile();
+			return !fileContent.isEmpty() && Boolean.parseBoolean(getStringFromFile());
+		} catch (Exception e) {
+			logger.warn("FilePersistentIndex:", e);
+		}
+		return false;
+	}
+
+	public boolean get() {
+		return persisted;
+	}
+
+	public boolean set(boolean newValue) {
+		persisted = newValue;
+		writeToFile(persisted);
+		return persisted;
+	}
+}
