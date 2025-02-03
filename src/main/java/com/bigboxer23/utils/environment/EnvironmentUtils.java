5aef7f0500f4e46549f5f3e0f59526e82f9b22a4
Matt Jones <bigboxer23@gmail.com>
2025-02-02 19:56:48 -0600

    feat: add ctof, ftoc utils

---
+++ src/main/java/com/bigboxer23/utils/environment/EnvironmentUtils.java
@@ -0,0 +1,15 @@
+package com.bigboxer23.utils.environment;
+
+/**
+ *
+ */
+public class EnvironmentUtils
+{
+	public static float celciusToFahrenheit(float celcius) {
+		return (celcius * 1.8f) + 32f;
+	}
+
+	public static double fahrenheitToCelsius(double fahrenheit) {
+		return (fahrenheit - 32) * 1.8;
+	}
+}
