9a106363e17c3c0dbe6d08a40f24cabdcd35fe57
Matt Jones <bigboxer23@gmail.com>
2025-02-18 20:42:26 -0600

    feat: add more fields to mdc for enhanced logging

--- src/main/java/com/bigboxer23/utils/logging/LoggingUtil.java
+++ src/main/java/com/bigboxer23/utils/logging/LoggingUtil.java
@@ -9,16 +9,32 @@ public class LoggingUtil {
 		addDeviceId(deviceId);
 	}
 
-	public static void addDeviceId(String deviceId) {
-		MDC.put("deviceId", deviceId);
+	public static MDC.MDCCloseable addDeviceId(String deviceId) {
+		return MDC.putCloseable("deviceId", deviceId);
 	}
 
 	public static void clearDeviceId() {
 		MDC.remove("deviceId");
 	}
 
-	public static void addMethod(String deviceId) {
-		MDC.put("method", deviceId);
+	public static MDC.MDCCloseable addTemperature(float temperature) {
+		return MDC.putCloseable("temperature", "" + temperature);
+	}
+
+	public static MDC.MDCCloseable addWatts(float watts) {
+		return MDC.putCloseable("watts", "" + watts);
+	}
+
+	public static MDC.MDCCloseable addHumidity(int humidity) {
+		return MDC.putCloseable("humidity", "" + humidity);
+	}
+
+	public static MDC.MDCCloseable addCO2(int co2) {
+		return MDC.putCloseable("co2", "" + co2);
+	}
+
+	public static MDC.MDCCloseable addMethod(String method) {
+		return MDC.putCloseable("method", method);
 	}
 
 	public static void clearContext() {
